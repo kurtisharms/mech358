6dcebf61fddf018b6e5f764d97a13d46d726766d
Kurtis Harms <emailkurtis@gmail.com>
2015-02-13 11:43:54 -0800

    Added more tests for varying values of h at L=100

--- lab2/problem2/problem2.m
+++ lab2/problem2/problem2.m
@@ -1,6 +1,6 @@
 %   problem2.m
-%   Author: Kurtis Harms
-%   Student #38764114
+%   Authors:    Kurtis Harms (38764114)
+%               Ecem 
 %   MECH 358 Lab 2
 %   Purpose: Problem 2, a convective heat transfer problem
 %   Required Files: solve_bvp.m
@@ -11,17 +11,15 @@ Pe_values = 1;
 h_values = [0.1,1];
 L_values = [10,50,100];
 
-colors = ['-r','b',':k','g'];
+colors = ['r','b','k','g','y'];
+linewidth = [1,1,2,2,2];
 
 T_bvs = [1 0];
 
 Pe = 1;
 
-
-
 i = 1;
-for L = L_values
-    
+for L = L_values    
     
     figure(i);
     hold on;
@@ -30,19 +28,19 @@ for L = L_values
     for h = h_values
         
         % Create and plot analytical solution
-        x = 0:0.01:L;
+        x = 0:0.001:L;
         y = exp(-Pe.*x);
         plot(x,y,colors(j));
         
         % Create and plot numerical solution
         T = solve_bvp(L,h,Pe,T_bvs(1),T_bvs(2));
         x = 0:h:L;
-        plot(x,T,colors(j+1));
+        plot(x,T,colors(j+1),'linewidth',linewidth(i));
 
         j = j+2;
     end
     
-    legend('Analytical Solution with h=0.1', ...
+    legend('Analytical Solution', ...
     'Numerical Solution with h=0.1', ...
     'Analytical Solution with h=1', ...
     'Numerical Solution with h=1');
@@ -60,18 +58,20 @@ hold on;
 
 i = 1;
 L = 100;
+h_values = [0.01,0.1,1,5,10];
 
 for h = h_values
     T = solve_bvp(L,h,Pe,T_bvs(1),T_bvs(2));
 
     x = 0:h:L;
-    plot(x,T,colors(i));
+    plot(x,T,colors(i),'linewidth',linewidth(i));
 
     i = i+1;
 end
 
 
 title('T(x) vs. x numerical solutions for L=100 and varying h');
+legend('h=0.01','h=0.1','1','5','10');
 xlabel('x')
 ylabel('(T(x)');
 
